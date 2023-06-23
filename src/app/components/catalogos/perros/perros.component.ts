f4f3e72eccabbb018220d1bec781ec50862a357c
Juan Echeverria <juan.echeverriatro@ug.edu.ec>
2023-06-23 00:32:41 -0500

    Diseño de detalle de producto

--- src/app/components/catalogos/perros/perros.component.ts
+++ src/app/components/catalogos/perros/perros.component.ts
@@ -42,15 +42,19 @@ export class PerrosComponent implements OnInit {
       {idProducto: 1, nombre: "Hueso de hule", descripcion: "Hueso de hule para perros",
       idCategoria: 1, precioVenta: 3, stock: 30, urlFoto: "./assets/img/prod-perros/P0001.jpg"},
       {idProducto: 2, nombre: "Collar de cuero", descripcion: "Collar de cuero para perros",
-      idCategoria: 1, precioVenta: 3, stock: 25, urlFoto: "./assets/img/prod-perros/P0002.jpg"},
+      idCategoria: 1, precioVenta: 3.3, stock: 25, urlFoto: "./assets/img/prod-perros/P0002.jpg"},
       {idProducto: 3, nombre: "Comida Pro-Can", descripcion: "Comida Para Perro Cachorro Raza Mediana Pro-Can",
-      idCategoria: 1, precioVenta: 3, stock: 6, urlFoto: "./assets/img/prod-perros/P0003.jpg"},
+      idCategoria: 1, precioVenta: 3, stock: 66, urlFoto: "./assets/img/prod-perros/P0003.jpg"},
       {idProducto: 4, nombre: "Cepillo para perros", descripcion: "Cepillo para perros",
-      idCategoria: 1, precioVenta: 3, stock: 9, urlFoto: "./assets/img/prod-perros/P0004.jpg"},
+      idCategoria: 1, precioVenta: 3.6, stock: 99, urlFoto: "./assets/img/prod-perros/P0004.jpg"},
       {idProducto: 5, nombre: "Perfume para mascotas", descripcion: "Perfume para mascotas",
-      idCategoria: 1, precioVenta: 3, stock: 10, urlFoto: "./assets/img/prod-perros/P0005.jpg"},
+      idCategoria: 1, precioVenta: 3, stock: 100, urlFoto: "./assets/img/prod-perros/P0005.jpg"},
       {idProducto: 6, nombre: "Cama para perros", descripcion: "Hueso de hule par perros",
-      idCategoria: 1, precioVenta: 3, stock: 25, urlFoto: "./assets/img/prod-perros/P0006.jpg"}
+      idCategoria: 1, precioVenta: 5.6, stock: 25, urlFoto: "./assets/img/prod-perros/P0006.jpg"},
+      {idProducto: 7, nombre: "Comida Dog Chow", descripcion: "Comida Para Perro Cachorro Raza Mediana Dog Chow",
+      idCategoria: 1, precioVenta: 8, stock: 40, urlFoto: "./assets/img/prod-perros/P0007.jpg"},
+      {idProducto: 8, nombre: "Comida Pedigree", descripcion: "Comida Para Perro Cachorro Raza Mediana Pedigree",
+      idCategoria: 1, precioVenta: 6.9, stock: 55, urlFoto: "./assets/img/prod-perros/P0008.jpg"}
     ]
   }
 }
