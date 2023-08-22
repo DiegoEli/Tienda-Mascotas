97d628539b9f385fc8b3d968669fe9adcef0bf2c
DiegoEli <diego.mendozam@ug.edu.ec>
2023-08-22 01:14:08 -0500

    add corrections

--- src/app/components/catalogos/conejos/conejos.component.ts
+++ src/app/components/catalogos/conejos/conejos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Producto } from 'src/app/interfaces/producto';
+// import { Producto } from 'src/app/interfaces/producto';
 
 @Component({
   selector: 'app-conejos',
@@ -7,9 +7,9 @@ import { Producto } from 'src/app/interfaces/producto';
   styleUrls: ['./conejos.component.css']
 })
 export class ConejosComponent implements OnInit{
-  listaFiltrada: Producto[] = [];
+  // listaFiltrada: Producto[] = [];
   categoriaSeleccionada: string = 'Todas';
-  listaProductosConejos: Producto[] = [];
+  // listaProductosConejos: Producto[] = [];
 
   ngOnInit(): void {
     ConejosComponent.constructor();
@@ -17,24 +17,24 @@ export class ConejosComponent implements OnInit{
   }
 
   constructor(){
-    this.listaProductosConejos = [
-      {idProducto: 1000, nombre: "Jaula Rabbit 100 el Grigia Gabbia Medium", descripcion: "Jaula para conejos de Ferplast",
-      idCategoria: 3, precioVenta: 89, stock: 15, urlFoto: "./assets/img/prod-conejos/C0001.png"},
-      {idProducto: 1001, nombre: "Cepillo Ferplast Spazzola Morbida Rod", descripcion: "Cepillo de Ferplast para roedores",
-      idCategoria: 3, precioVenta: 5.60, stock: 20, urlFoto: "./assets/img/prod-conejos/C0002.png"},
-      {idProducto: 1002, nombre: "Juguete Ferplast Goodb Nat Bag Carrot", descripcion: "Juguete para mordisquear para roedores",
-      idCategoria: 2, precioVenta: 4.30, stock: 25, urlFoto: "./assets/img/prod-conejos/C0003.png"},
-      {idProducto: 1003, nombre: "Botella de Agua Living + Water Bottle", descripcion: "Botella de Agua para conejos",
-      idCategoria: 1, precioVenta: 10.55, stock: 20, urlFoto: "./assets/img/prod-conejos/C0004.png"},
-      {idProducto: 1004, nombre: "Juguete Ferplast PA 4756 Vegetale Colorato", descripcion: "Juguete para conejos",
-      idCategoria: 2, precioVenta: 8, stock: 35, urlFoto: "./assets/img/prod-conejos/C0005.png"},
-      {idProducto: 6, nombre: "Voltrega Cama para Cobaya 200 GR para Conejos", descripcion: "Cama cálida y suave para conejos",
-      idCategoria: 2, precioVenta: 15, stock: 15, urlFoto: "./assets/img/prod-conejos/C0006.png"},      
-      {idProducto: 6, nombre: "Jr Farm Snacks Anillos de Verdura para Conejos", descripcion: "Alimento complementario para todos los conejos",
-      idCategoria: 1, precioVenta: 3, stock: 30, urlFoto: "./assets/img/prod-conejos/C0007.png"},      
-      {idProducto: 6, nombre: "Cunipic Champú de Biotina para Conejos", descripcion: "Champú de Biotina para Conejos",
-      idCategoria: 3, precioVenta: 13, stock: 15, urlFoto: "./assets/img/prod-conejos/C0008.png"}
-    ]
+    // this.listaProductosConejos = [
+    //   {idProducto: 1000, nombre: "Jaula Rabbit 100 el Grigia Gabbia Medium", descripcion: "Jaula para conejos de Ferplast",
+    //   idCategoria: 3, precioVenta: 89, stock: 15, urlFoto: "./assets/img/prod-conejos/C0001.png"},
+    //   {idProducto: 1001, nombre: "Cepillo Ferplast Spazzola Morbida Rod", descripcion: "Cepillo de Ferplast para roedores",
+    //   idCategoria: 3, precioVenta: 5.60, stock: 20, urlFoto: "./assets/img/prod-conejos/C0002.png"},
+    //   {idProducto: 1002, nombre: "Juguete Ferplast Goodb Nat Bag Carrot", descripcion: "Juguete para mordisquear para roedores",
+    //   idCategoria: 2, precioVenta: 4.30, stock: 25, urlFoto: "./assets/img/prod-conejos/C0003.png"},
+    //   {idProducto: 1003, nombre: "Botella de Agua Living + Water Bottle", descripcion: "Botella de Agua para conejos",
+    //   idCategoria: 1, precioVenta: 10.55, stock: 20, urlFoto: "./assets/img/prod-conejos/C0004.png"},
+    //   {idProducto: 1004, nombre: "Juguete Ferplast PA 4756 Vegetale Colorato", descripcion: "Juguete para conejos",
+    //   idCategoria: 2, precioVenta: 8, stock: 35, urlFoto: "./assets/img/prod-conejos/C0005.png"},
+    //   {idProducto: 6, nombre: "Voltrega Cama para Cobaya 200 GR para Conejos", descripcion: "Cama cálida y suave para conejos",
+    //   idCategoria: 2, precioVenta: 15, stock: 15, urlFoto: "./assets/img/prod-conejos/C0006.png"},      
+    //   {idProducto: 6, nombre: "Jr Farm Snacks Anillos de Verdura para Conejos", descripcion: "Alimento complementario para todos los conejos",
+    //   idCategoria: 1, precioVenta: 3, stock: 30, urlFoto: "./assets/img/prod-conejos/C0007.png"},      
+    //   {idProducto: 6, nombre: "Cunipic Champú de Biotina para Conejos", descripcion: "Champú de Biotina para Conejos",
+    //   idCategoria: 3, precioVenta: 13, stock: 15, urlFoto: "./assets/img/prod-conejos/C0008.png"}
+    // ]
   }
   
 
@@ -43,15 +43,15 @@ export class ConejosComponent implements OnInit{
 
   filtrarPorCategoria(categoria: string) {
     this.categoriaSeleccionada = categoria;
-    this.listaFiltrada= [...this.listaProductosConejos];
+    // this.listaFiltrada= [...this.listaProductosConejos];
 
     // Filtrar la lista de productos según la categoría seleccionada
     if (categoria === 'Todas') {
       // Mostrar todos los productos
-      this.listaFiltrada ;
+      // this.listaFiltrada ;
     } else {
       // Filtrar por categoría
-      this.listaFiltrada = this.listaFiltrada.filter(producto => producto.idCategoria === this.getIdCategoria(categoria));
+      // this.listaFiltrada = this.listaFiltrada.filter(producto => producto.idCategoria === this.getIdCategoria(categoria));
     }
   }
 
@@ -71,6 +71,6 @@ export class ConejosComponent implements OnInit{
 
   mostrarProducto(){
     this.categoriaSeleccionada = 'Todas';
-    this.listaFiltrada= [...this.listaProductosConejos];
+    // this.listaFiltrada= [...this.listaProductosConejos];
   }
 }
--- src/app/components/catalogos/gatos/gatos.component.ts
+++ src/app/components/catalogos/gatos/gatos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Producto } from 'src/app/interfaces/producto';
+//import { Producto } from 'src/app/interfaces/producto';
 
 @Component({
   selector: 'app-gatos',
@@ -8,54 +8,54 @@ import { Producto } from 'src/app/interfaces/producto';
 })
 export class GatosComponent implements OnInit   {
 
-  listaProductosGatos: Producto[] = [];
-  listaFiltrada: Producto[] = [];
+  // listaProductosGatos: Producto[] = [];
+  // listaFiltrada: Producto[] = [];
   categoriaSeleccionada: string = 'Todas';
 
   constructor(){
-    this.listaProductosGatos = [
-      {idProducto: 1000, nombre: "Comida para gato Happy Cat", descripcion: "Almentación PREMIUM para gatos cachorros.",
-      idCategoria: 1, precioVenta: 14, stock: 30, urlFoto: "./assets/img/prod-gatos/comida_gato_1.png"},
-      {idProducto: 1001, nombre: "Alimento Húmedo Para Gatitos Cachorros", descripcion: "Collar de cuero para perros",
-      idCategoria: 1, precioVenta: 3.80, stock: 25, urlFoto: "./assets/img/prod-gatos/comida_gato_2.png"},
-      {idProducto: 1002, nombre: "Alimento Premium para Gatos y Gatitos ", descripcion: "Comida Para Perro Cachorro Raza Mediana Pro-Can",
-      idCategoria: 1, precioVenta: 10.00, stock: 6, urlFoto: "./assets/img/prod-gatos/comida_gato_3.png"},
-      {idProducto: 1003, nombre: "Alimento Para Gatos Castrados", descripcion: "Cepillo para perros",
-      idCategoria: 1, precioVenta: 17, stock: 9, urlFoto: "./assets/img/prod-gatos/comida_gato_4.png"},
-      {idProducto: 1004, nombre: "Ratones 3 Unidades Juguete Clásico", descripcion: "Perfume para mascotas",
-      idCategoria: 2, precioVenta: 1, stock: 20, urlFoto: "./assets/img/prod-gatos/juguete_gato_01.png"},
-      {idProducto: 6, nombre: "Juguetes Variados de Gatos", descripcion: "Hueso de hule par perros",
-      idCategoria: 2, precioVenta: 10, stock: 10, urlFoto: "./assets/img/prod-gatos/juguete_gato_02.png"},      
-      {idProducto: 1005, nombre: "Juguete Porfiado Premium Con Plumas", descripcion: "Juguete porfiado premium con plumas para gatos",
-      idCategoria: 2, precioVenta: 4.50, stock: 10, urlFoto: "./assets/img/prod-gatos/juguete_gato_03.png"},      
-      {idProducto: 1006, nombre: "Caña De Pescar Telescópica Juguete Para Gato", descripcion: "Caña de pescar telescopica juguete para gato",
-      idCategoria: 2, precioVenta: 10.00, stock: 10, urlFoto: "./assets/img/prod-gatos/juguete_gato_04.png"}
-    ]
+    // this.listaProductosGatos = [
+    //   {idProducto: 1000, nombre: "Comida para gato Happy Cat", descripcion: "Almentación PREMIUM para gatos cachorros.",
+    //   idCategoria: 1, precioVenta: 14, stock: 30, urlFoto: "./assets/img/prod-gatos/comida_gato_1.png"},
+    //   {idProducto: 1001, nombre: "Alimento Húmedo Para Gatitos Cachorros", descripcion: "Collar de cuero para perros",
+    //   idCategoria: 1, precioVenta: 3.80, stock: 25, urlFoto: "./assets/img/prod-gatos/comida_gato_2.png"},
+    //   {idProducto: 1002, nombre: "Alimento Premium para Gatos y Gatitos ", descripcion: "Comida Para Perro Cachorro Raza Mediana Pro-Can",
+    //   idCategoria: 1, precioVenta: 10.00, stock: 6, urlFoto: "./assets/img/prod-gatos/comida_gato_3.png"},
+    //   {idProducto: 1003, nombre: "Alimento Para Gatos Castrados", descripcion: "Cepillo para perros",
+    //   idCategoria: 1, precioVenta: 17, stock: 9, urlFoto: "./assets/img/prod-gatos/comida_gato_4.png"},
+    //   {idProducto: 1004, nombre: "Ratones 3 Unidades Juguete Clásico", descripcion: "Perfume para mascotas",
+    //   idCategoria: 2, precioVenta: 1, stock: 20, urlFoto: "./assets/img/prod-gatos/juguete_gato_01.png"},
+    //   {idProducto: 6, nombre: "Juguetes Variados de Gatos", descripcion: "Hueso de hule par perros",
+    //   idCategoria: 2, precioVenta: 10, stock: 10, urlFoto: "./assets/img/prod-gatos/juguete_gato_02.png"},      
+    //   {idProducto: 1005, nombre: "Juguete Porfiado Premium Con Plumas", descripcion: "Juguete porfiado premium con plumas para gatos",
+    //   idCategoria: 2, precioVenta: 4.50, stock: 10, urlFoto: "./assets/img/prod-gatos/juguete_gato_03.png"},      
+    //   {idProducto: 1006, nombre: "Caña De Pescar Telescópica Juguete Para Gato", descripcion: "Caña de pescar telescopica juguete para gato",
+    //   idCategoria: 2, precioVenta: 10.00, stock: 10, urlFoto: "./assets/img/prod-gatos/juguete_gato_04.png"}
+    // ]
   }
   ngOnInit() {
     GatosComponent.constructor();
 
-    this.mostrarProducto(); // Llamar al método mostrarProducto() al cargar el componente
+    //this.mostrarProducto(); // Llamar al método mostrarProducto() al cargar el componente
   }
 
-  filtrarPorCategoria(categoria: string) {
-    this.categoriaSeleccionada = categoria;
-    this.listaFiltrada= [...this.listaProductosGatos];
+  // filtrarPorCategoria(categoria: string) {
+  //   this.categoriaSeleccionada = categoria;
+  //   this.listaFiltrada= [...this.listaProductosGatos];
 
-    // Filtrar la lista de productos según la categoría seleccionada
-    if (categoria === 'Todas') {
-      // Mostrar todos los productos
-      this.listaFiltrada ;
-    } else {
-      // Filtrar por categoría
-      this.listaFiltrada = this.listaFiltrada.filter(producto => producto.idCategoria === this.getIdCategoria(categoria));
-    }
-  }
+  //   // Filtrar la lista de productos según la categoría seleccionada
+  //   if (categoria === 'Todas') {
+  //     // Mostrar todos los productos
+  //     this.listaFiltrada ;
+  //   } else {
+  //     // Filtrar por categoría
+  //     this.listaFiltrada = this.listaFiltrada.filter(producto => producto.idCategoria === this.getIdCategoria(categoria));
+  //   }
+  // }
 
-  mostrarProducto(){
-    this.categoriaSeleccionada = 'Todas';
-    this.listaFiltrada= [...this.listaProductosGatos];
-  }
+  // mostrarProducto(){
+  //   this.categoriaSeleccionada = 'Todas';
+  //   this.listaFiltrada= [...this.listaProductosGatos];
+  // }
 
 
   getIdCategoria(categoria: string): number {
